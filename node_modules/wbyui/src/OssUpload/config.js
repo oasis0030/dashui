9a22089f37652513e36fbbe02dea6ca24de2b9f5
Murphite <lzbabd@gmail.com>
2024-03-05 13:58:22 +0800

    del more

--- node_modules/wbyui/src/OssUpload/config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by lzb on 2019-01-24.
- */
-export const BUCKET_MAP = {
-  'public': '/api/common-file/file/v1/uploadPubBucket',
-  'private': '/api/common-file/file/v1/uploadPriBucket',
-  'public-cdn': '/api/common-file/file/v1/uploadPubCdnBucket'
-};
